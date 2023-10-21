53da608da3ec01e3cffcfc225b401b6253c6b233
marcoswlrich <marcosapw.dev@gmail.com>
2023-10-20 22:58:41 -0400

    Add package SecretManager

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	lambda "github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/marcoswlrich/ecobrutus/awsgo"
+	"github.com/marcoswlrich/ecobrutus/bd"
+	"github.com/marcoswlrich/ecobrutus/models"
 )
 
 func main() {
@@ -25,6 +27,25 @@ func ExecuteLambda(
 	if !ValidateParameters() {
 		fmt.Println("Erro nos parametros, deve-se enviar 'SecretName'")
 		err := errors.New("Erro nos parametros deve-se enviar SecretName")
+		return event, err
+	}
+
+	var data models.SignUp
+
+	for row, att := range event.Request.UserAttributes {
+		switch row {
+		case "email":
+			data.UserEmail = att
+			fmt.Println("Email = " + data.UserEmail)
+		case "sub":
+			data.UserUUID = att
+			fmt.Println("Sub = " + data.UserUUID)
+		}
+	}
+
+	err := bd.ReadSecret()
+	if err != nil {
+		fmt.Println("Erro ao ler Secret " + err.Error())
 		return event, err
 	}
 }
